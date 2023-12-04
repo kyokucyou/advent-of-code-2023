15549e66d9da7c0618d0ec66071ecc11cd856bf4
kyokucyou <kyokucyou@yahoo.com>
2023-12-04 15:29:04 +0100

    day 4: removed unneeded variable

--- perl/day04b.pl
+++ perl/day04b.pl
@@ -2,25 +2,22 @@
 
 use strict;
 
-my (@scores, @matching);
+my @matching;
 while (<>) {
     my @nums = map int, /\d+/g;
     my @wins = splice @nums, 1, 10;
     my @has = splice @nums, 1;
-    my $score = 0.5;
     my $matched = 0;
     for my $win (@wins) {
         for my $num (@has) {
             next if $win != $num;
-            $score *= 2;
             ++$matched;
             last;
         }
     }
-    push @scores, int $score;
     push @matching, $matched;
 }
-my $cards = $#scores + 1;
+my $cards = $#matching + 1;
 my @avail = (1) x $cards;
 while (1) {
     my $winning = 0;
